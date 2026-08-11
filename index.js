d1613383ed7eac9cede1929c25688cc450470568
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-08-10 22:44:13 -0600

    One audio drum added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let numberOfDrums = document.querySelectorAll(".drum").length;
 
 for(let i = 0; i <numberOfDrums; i++){
     document.querySelectorAll('.drum')[i].addEventListener("click", function(){
-        alert('I got clicked!');
+        let audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
     });
 }
 
