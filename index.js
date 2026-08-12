7e7780543f7d21e2f0df9e395c59262e962143ce
EddieGilXD <119008695+EddieGilXD@users.noreply.github.com>
2026-08-11 22:19:48 -0600

    Working ont the DOM side

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let numberOfDrums = document.querySelectorAll(".drum").length;
 
 for(let i = 0; i <numberOfDrums; i++){
     document.querySelectorAll('.drum')[i].addEventListener("click", function(){
-        let audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        this.style.color = 'white';
     });
 }
 
 
-
+let audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
